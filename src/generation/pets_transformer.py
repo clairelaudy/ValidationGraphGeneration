68b5c88aeed72139f968883aed0da074188d0162
Claire Laudy <claire.laudy@pasteur.fr>
2026-01-29 11:18:13 +0100

    fix(pets_transformer): Fixes the classes names and removes prints in the pets _transformer script.

--- src/generation/pets_transformer.py
+++ src/generation/pets_transformer.py
@@ -6,19 +6,17 @@ class pets_transformer(ontoweaver.transformer.Transformer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 #        self.declare = ontoweaver.base.Declare(raise_errors = kwargs["raise_errors"])
-        self.declare_types.make_node_class("Cat")
-        self.declare_types.make_node_class("Dog")
-        self.declare_types.make_node_class("Animal")
-        print(dir(owtypes))
-        self.declare_types.make_edge_class("has_pet", getattr(owtypes, "person"), getattr(owtypes, "Animal"))
+        self.declare_types.make_node_class("cat")
+        self.declare_types.make_node_class("dog")
+        self.declare_types.make_node_class("animal")
+        self.declare_types.make_edge_class("hasPet", getattr(owtypes, "person"), getattr(owtypes, "animal"))
 
     def __call__(self, row, i):
         pets = str(row["pets"])
         if pets != "nan":
-            print("pets = ", pets)
             for pet in pets.split(";"):
                 node_value, node_type = pet.split("(")
-                node_type = node_type.replace(")", "").capitalize()
-                yield node_value, getattr(owtypes, "has_pet"), getattr(owtypes, node_type), None
+                node_type = node_type.replace(")", "")#.capitalize()
+                yield node_value, getattr(owtypes, "hasPet"), getattr(owtypes, node_type), None
         
         
