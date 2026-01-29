fa5bc455ad7d19f07ed4267e17abb7e3b442255e
Claire Laudy <claire.laudy@pasteur.fr>
2026-01-29 15:22:08 +0100

    fix(postprocess ontology scaling): catches an error of results reading in case the ontology was too big for BC/Ontoweaver to handle.

--- src/postprocessing/process_reasoner_time.py
+++ src/postprocessing/process_reasoner_time.py
@@ -15,6 +15,7 @@ import matplotlib.pyplot as plt
 from os import walk
 from pprint import pprint
 import re
+import logging
 
 def find_time(s):
     list_times = re.findall(r'[0-9]+', s)
@@ -42,13 +43,13 @@ if __name__ == "__main__":
     # data.columns = ['dataset id', 'scenario', 'nb of classes', 'nb of individuals', 'nb of nodes', 'reasoning time', 'data generation time']
     row_list = []
 
-    with open(args.output_csv, "w"):
-        files = []
-        result = ""
-        for (dirpath, dirnames, filenames) in walk(expe_dir):
-            files.extend(filenames)
-            break
-        for f in files:
+    files = []
+    result = ""
+    for (dirpath, dirnames, filenames) in walk(expe_dir):
+        files.extend(filenames)
+        break
+    for f in files:
+        try:
             if f.endswith(".log"):
                 file = expe_dir+f
                 try:
@@ -61,21 +62,23 @@ if __name__ == "__main__":
                     data = {}
                     for line in result.splitlines():
                         lines.append(line.decode())
-                    pprint(lines)
+                    #pprint(lines)
                     data['scenario'] = lines[0].partition("Scenario: ")[2]
                     nb_classes = int(lines[1].partition(" owl:Class: ")[2])
                     data['nb of classes'] = nb_classes
                     nb_indiv = int(lines[2].partition(" owl:NamedIndividual: ")[2])
                     data['nb of individuals'] = nb_indiv
                     data['nb of nodes'] = nb_classes+nb_indiv
-                    
+                
                     data['reasoning time'] = find_time(lines[6])
                     data['data gegeration time'] = find_time(lines[9]) 
-                    
+                
                     row_list.append(data)
+        except:
+            logging.warning(f"Couldn't read results from `{f}'")
 
     data = pd.DataFrame(row_list)
-    print("datadframe = ")
+    #print("datadframe = ")
     pprint(data)
     data.to_csv("".join([args.expe_dir, "test.csv"]))
     data.plot(kind='scatter', x='nb of nodes', y='reasoning time')
